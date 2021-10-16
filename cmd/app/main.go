a4f60b5b4127bc26a0c2d90d33c4d0bbf35fd7f3
Archit <gupta.archit13@gmail.com>
2021-10-16 16:17:33 +0530

    added logger and configs

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/go-kit/kit/log"
+	"go-microservice-boilerplate/pkg/utl/config"
+	stdlog "log"
+	"os"
+)
 
 func main() {
 	fmt.Println("Can y'all see this too?")
+
+	// Set up logger interface
+	var logger log.Logger
+	logger = log.NewJSONLogger(log.NewSyncWriter(os.Stderr))
+	stdlog.SetOutput(log.NewStdlibAdapter(logger))
+	logger = log.With(logger, "ts", log.DefaultTimestampUTC, "loc", log.DefaultCaller)
+
+	_ = config.GetConfig()
+
 }
